d870e2556dd97373a51b14b2a9cd18e193fb3af3
Aleksandr Spiian <aleksandr.spiian@icloud.com>
2024-01-29 14:34:51 +0100

    added assets for UI, added cube prefab, mechanics of figure rotation, ssmooth spawn of cubes

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity;
+    void Update()
+    {
+        if (!Input.GetMouseButton(0)) return;
+        
+        var rotationX = Input.GetAxis("Mouse X") * _sensitivity;
+        var rotationY = Input.GetAxis("Mouse Y") * _sensitivity;
+            
+        transform.RotateAround(transform.position, Vector3.up, -rotationX);
+        transform.RotateAround(transform.position, Vector3.right, rotationY);
+    }
+    
+}
